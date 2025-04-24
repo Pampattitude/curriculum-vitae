f14cc29eb945bd535aba8fba4c83c33220cd7bf3
Guillaume Delahodde <pampattitude@gmail.com>
2025-04-24 13:37:49 +0200

    Update TagGauge.tsx

--- src/components/TagGauge.tsx
+++ src/components/TagGauge.tsx
@@ -13,7 +13,7 @@ const hashCode = (str: string): number => {
 };
 
 const STRENGTH_BG = 600 as const;
-const STRENGTH_BG_LIGHT = 600 as const;
+const STRENGTH_BG_LIGHT = 700 as const;
 const STRENGTH_TEXT = 300 as const;
 const STRENGTH_TEXT_LIGHT = 100 as const;
 const STRENGTH_WHITE_TEXT = 100 as const;
@@ -98,7 +98,7 @@ export const TagGauge = ({
     <div
       {...rest}
       className={[
-        "text-xs min-w-8 inline-flex relative rounded-md overflow-hidden saturate-50",
+        "text-xs min-w-8 inline-flex relative rounded-md overflow-hidden saturate-[75%]",
         tooltip ? "cursor-help" : "cursor-default",
         className,
       ]
