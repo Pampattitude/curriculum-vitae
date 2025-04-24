8657bd42741a7d4563ab41aa6594323a1c16d8f3
Guillaume Delahodde <pampattitude@gmail.com>
2025-04-24 13:48:50 +0200

    improved main content side

--- src/layout/Main/Content.tsx
+++ src/layout/Main/Content.tsx
@@ -3,7 +3,9 @@ import { Experiences } from "../../content/Experiences";
 import "./Content.css";
 
 export const Content = () => (
-  <section className="content p-4 grow-1 shrink-1">
-    <Experiences />
+  <section className="content p-4 grow-1 shrink-1 print:p-0">
+    <div className="max-w-4xl m-auto print:max-w-none">
+      <Experiences />
+    </div>
   </section>
 );
