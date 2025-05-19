5c0c84833cd58e69714751340f1fc2398363836f
Pampattitude <pampattitude@gmail.com>
2025-05-19 12:04:39 +0200

    testing srcset / sizes for brand

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -6,10 +6,10 @@ export const Brand = ({
 }: Pick<HTMLImageElement, "src" | "alt"> & Partial<HTMLImageElement>) => (
   <img
     {...(rest as React.PropsWithChildren)}
-    width="1em"
-    height="1em"
     className={["inline-block h-[1em]", className].filter((c) => c).join(" ")}
     src={src}
+    srcSet={`${src} 1w`}
+    sizes="((min-width: 1em) and (max-width: 1em)) 1em"
     alt={alt}
   />
 );
